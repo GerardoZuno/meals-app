cc96b21b0e47b5075ee327026a4c540d3f419ba3
Gerardo Zuno <gerardozunounitec@gmail.com>
2021-07-12 16:47:15 -0500

    Add structure navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export const NavBar = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="navbar">
+      <h1>
+        Meals <span>App</span>
+      </h1>
+
+      <div className="navbar-heading">
+        <div className="navbar-links">
+          <ul>
+            <li>Home</li>
+            <li>Categories</li>
+            <li>Random</li>
+          </ul>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
